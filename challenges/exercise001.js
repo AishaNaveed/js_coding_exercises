34adc8a18e64d365d59beb02626089281fe8f6c9
Aisha Naveed <aisha@ailogica.com>
2021-09-27 00:17:16 +0100

    implemented few tests of exercise 001

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,35 +1,44 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  
+  return firstName.charAt(0)+ "."+ lastName.charAt(0);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  
+  return originalPrice + (originalPrice * vatRate / 100);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+
+  return originalPrice - (originalPrice * reduction / 100);
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  
+  const size = str.length;   
+    if(size % 2 == 0)
+        return str.substring((size/2) - 1, (size/2) + 1);
+    else
+        return str.substring((size-1) / 2, (size-1) / 2 + 1);
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+// split() will return a new array
+    return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
@@ -49,7 +58,15 @@ function getMeanScore(scores) {
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  // Add your code here!
+  
+  if((n % 3) == 0 && (n % 5) == 0)
+        return "fizzbuzz";
+    else if(n % 5 === 0)
+        return "buzz";
+    else if(n % 3 === 0 )
+        return "fizz";
+    else
+        return n;   
 }
 
 module.exports = {
