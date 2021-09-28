9a9937c2e6354e2ef19db3e05eb07d443090eb1a
Aisha Naveed <aisha@ailogica.com>
2021-09-28 18:39:53 +0100

    implemented some testcases for exercise004

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,32 +1,96 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  let temp = 0;
+  let arr = new Array();
+  for(let i = 0; i < nums.length; i++)
+  {
+      if(nums[i] < 1)
+      {
+          arr[temp] = nums[i];
+          temp++;
+      }
+  }
+  return arr;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let temp = 0; 
+  let arr = new Array();
+  for(let i = 0; i < names.length; i++)
+  {
+      if(names[i].charAt(0) == char)
+      {
+          arr[temp] = names[i];
+          temp++;
+      }
+  }
+  return arr;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let temp=0;
+  let arr = new Array();
+  for(let i = 0; i < words.length; i++)
+  {
+      if(words[i].startsWith("to ") == true)
+      {
+          arr[temp] = words[i];
+          temp++;
+      }
+  }
+  return arr;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let temp = 0;
+    let arr = new Array();
+    for(let i = 0; i < nums.length; i++)
+    {
+        if((nums[i] % 1) == 0)
+        {
+            arr[temp] = nums[i];
+            temp++;
+        }
+    }
+    return arr;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  let temp = 0;
+    let arr = new Array();
+    for(let i = 0; i < users.length; i++)
+    {
+        arr[temp] = users[i].data.city.displayName;
+        temp++;
+    } 
+    return arr;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let temp = 0;
+    let arr = new Array();
+    for(let i = 0; i < nums.length; i++)
+    {
+        let num = Math.sqrt(nums[i]);
+        if(num % 1 != 0)
+        {
+            arr[temp] = num.toFixed(2);
+            temp++;       
+        }
+        else
+        {
+            arr[temp] = num;
+            temp++;
+        }
+    }
+    return arr;
 }
 
 function findSentencesContaining(sentences, str) {
